58f03df92847e494a8b51c66168b4e5ece6efd0f
Tobias Schmidt <bulkmail@zock.at>
2021-12-12 14:11:02 +0100

    Add debug-callback mechanism to CNeuron

--- Neuron.cpp
+++ Neuron.cpp
@@ -15,12 +15,25 @@ void CNeuron::stimulate(float stimulus)
 {
 	excitement+=stimulus;
 	++stimCount;
+        debug(NDBG_REASON::DBG_STIMULATE);
 }
 
 void CNeuron::run()
 {
+  debug(NDBG_REASON::DBG_RUN);
   for(std::vector<CAxon*>::iterator it=axon.begin();it!=axon.end();++it){
 	(*it)->stimulate(excitement);
   }
   excitement=0.0f;
+}
+
+void CNeuron::debug(unsigned int reason)
+{
+#ifdef _DEBUG
+   for(std::vector<NeuronDebugHandler_t*>::iterator it=dbg.begin();it!=dbg.end();++it){
+       if((*it)->mask&reason){
+       		(*it)->fn(reason,(*it)->pData,this);
+       }
+  }
+#endif
 }
--- Neuron.h
+++ Neuron.h
@@ -2,8 +2,26 @@
 #define __NEURON_H
 
 #include <vector>
+#include <string>
 
 class CAxon;
+class CNeuron;
+
+typedef enum neuronDebugReason{
+        DBG_STIMULATE=1,
+        DBG_RUN=2,
+
+        DBG_UNKNOWN=1024
+};
+
+typedef void neuronDebugCallback(neuronDebugReason reason, void* pData, CNeuron *pNeuron);
+
+typedef struct {
+        unsigned int mask;
+        neuronDebugCallback *fn;
+        void *pData;
+}DebugHandler_t;
+
 
 __declspec(dllexport) class CNeuron{
 	public:
